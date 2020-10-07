6dbc5d23d90be7f29d5aa8062960204d2c7f9b1f
ankitbeniwal <ab.ankitbeniwal@gmail.com>
2020-10-07 22:35:05 +0530

    :zap: Added Initial Spring Structure

---
+++ src/test/java/app/QuestCase/QuestCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package app.QuestCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
